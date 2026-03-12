0037f88659e265d636182c25cce491965a2763b8
Shreya Tyagi <shreyaatyagii@gmail.com>
2026-03-13 00:42:25 +0530

    follow unfollow feature

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 dns.setDefaultResultOrder("ipv4first");
 require('dotenv').config()
+ 
 const app=require("./src/app")
 const connectToDatabase=require("./src/config/databse")
 
--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -31,6 +31,7 @@ async function registerController(req, res){
     const token = jwt.sign(
         {
             id: user._id,
+            username: user.username
              
         },
         process.env.JWT_SECRET,{expiresIn:"1d"}
@@ -81,7 +82,7 @@ async function loginController(req, res){
     }
 
      const token = jwt.sign({
-        id: user._id,
+        id: user._id, username: user.username
     }, process.env.JWT_SECRET,{expiresIn:"1d"})
 
     res.cookie("token", token)
